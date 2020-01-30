9d22131b2a24126dea9053b8cfac903a45daa29f
Eduardo Pereira <edu.eabp@gmail.com>
2020-01-30 12:54:28 -0300

    setting up heroku scripts

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
+const proxy = require("http-proxy-middleware");
 
 const app = express();
+
+app.use(proxy('/api/**', { target: 'http://localhost:5000' }));
 
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'products.json'));
 })
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('server running on port 3001');
 })
