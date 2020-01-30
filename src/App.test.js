87a91dad1209dcdcd9f41b6edd3c81fbf627abf1
Eduardo Pereira <edu.eabp@gmail.com>
2020-01-30 19:57:57 -0300

    add test of render app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+it('renders App correctly', () => {
+  const tree = TestRenderer
+    .create(<Provider store={store}>
+      <App />
+    </Provider>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
