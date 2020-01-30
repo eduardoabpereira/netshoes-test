92e68c7abf8c36960e2deb083249ecc3b8ca7662
Eduardo Pereira <edu.eabp@gmail.com>
2020-01-30 13:44:10 -0300

    add header json to api endpoint
    add body-parser

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
-const proxy = require("http-proxy-middleware");
+// const proxy = require("http-proxy-middleware");
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
-app.use(proxy('/api/**', { target: 'https://netshoes-frontend-edu.herokuapp.com' }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(proxy('/api/**', { target: 'https://netshoes-frontend-edu.herokuapp.com' }));
 
 const baseDir = `${__dirname}/build/`;
 app.use(express.static(`${baseDir}`));
@@ -13,6 +16,7 @@ app.use(express.static(`${baseDir}`));
 app.get('/', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 
 app.get('/api/products', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
   res.sendFile(path.join(__dirname, 'products.json'));
 })
 
