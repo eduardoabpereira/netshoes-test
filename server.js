9d6059bca363b69b413c7adc7bef9a972112d0f5
Eduardo Pereira <edu.eabp@gmail.com>
2020-01-30 13:19:43 -0300

    change server to correct use of react

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const app = express();
 
 app.use(proxy('/api/**', { target: 'http://localhost:5000' }));
 
+const baseDir = `${__dirname}/build/`;
+app.use(express.static(`${baseDir}`));
+
+app.get('/', (req,res) => res.sendFile('index.html' , { root : baseDir }))
+
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'products.json'));
 })
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log('server running on port 3001');
+  console.log(`server running on port ${process.env.PORT || 3001}`);
 })
