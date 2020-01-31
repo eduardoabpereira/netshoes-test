1bc185ea210483b600698403a57732c3388d1d0e
Eduardo Pereira <edu.eabp@gmail.com>
2020-01-31 00:46:03 -0300

    move data to public folder
    add local env
    
    change api call

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,7 +1,7 @@
 export const getProducts = () => {
   return (dispatch) => {
     dispatch({ type: 'GET_PRODUCTS_BEGIN' });
-    return fetch('/api/products')
+    return fetch(`${process.env.REACT_APP_API_URL}/api/products`)
       .then(res => res.json())
       .then(response => dispatch({ type: 'GET_PRODUCTS_SUCCESS', payload: response.products }))
   }
